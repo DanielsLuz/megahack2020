9f47146d2c4c9d30ec56187aef646fdf5782f9bf
Daniel Luz <danielluz1995@gmail.com>
2020-07-04 19:57:34 -0300

    Add TimeSlot entity.

---
+++ app/models/time_slot.rb
@@ -0,0 +1,16 @@
+class TimeSlot
+  include Mongoid::Document
+
+  DEFAULT_DURATION = 30.minutes
+
+  field :start_time, type: Time
+  field :duration, type: ActiveSupport::Duration, default: DEFAULT_DURATION
+  field :total_capacity, type: Integer
+  field :current_usage, type: Integer
+
+  belongs_to :owner
+
+  def to_s
+    start_time.strftime('%H:%M')
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Order.destroy_all
 Owner.destroy_all
 
-Owner.create(
+owner = Owner.create(
   name: 'Shawee Shopping',
   usable_tables_count: 15,
   reservations_start_time: '08:00',
@@ -66,3 +66,12 @@ Owner.create(
   ]
 )
 
+total_capacity = owner.slots_quantity
+owner.reservation_hours.each do |start_time|
+  TimeSlot.create!(
+    start_time: start_time,
+    total_capacity: total_capacity,
+    current_usage: total_capacity,
+    owner: owner
+  )
+end
