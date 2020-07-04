12fc7badb3af865fec2713311d1d9b2a23a0f87b
Daniel Luz <danielluz1995@gmail.com>
2020-07-03 21:28:51 -0300

    Initial menu.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#B33641',
+        secondary: '#66141B',
+        tertiary: '#1A4C66',
+      }
+    },
   },
   variants: {},
   plugins: []
