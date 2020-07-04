e4e58beac6368dc37f62c9db1d2b713852591bee
Daniel Luz <danielluz1995@gmail.com>
2020-07-03 22:31:55 -0300

    Initial links and controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :customers, only: :index
+  resources :owners, only: :index
+  resources :orders, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 end
