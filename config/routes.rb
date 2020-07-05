6ca25a48c523a31d21dbe6a8bde821f5d1f5cb00
Daniel Luz <danielluz1995@gmail.com>
2020-07-04 23:31:37 -0300

    Cleanup routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
     get 'menu'
     get 'orders'
   end
-  get 'code' => "restaurants#code", as: :restaurant_code
-  post 'check_code' => "restaurants#check_code", as: :restaurant_check_code
+
+  namespace 'restaurants' do
+    get 'code'
+    post 'check_code'
+  end
 
   resources :owners, only: [:show, :create, :edit, :update] do
     get 'restaurants', to: 'owners#new_restaurant'
@@ -15,12 +18,10 @@ Rails.application.routes.draw do
     patch 'restaurants/:id', to: 'owners#update_restaurant'
     delete 'restaurants/:id', to: 'owners#destroy_restaurant'
     get 'restaurants/:id/edit', to: 'owners#edit_restaurant', as: :edit_restaurant
-    get 'time_slots', on: :collection
   end
 
   post 'owners/tables', to: 'owners#tables_create'
 
-  post 'orders/check_code' => 'orders#check_code'
   resources :orders, only: [:index, :show, :create, :update] do
     get 'time_slots', on: :collection
   end
